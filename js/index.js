1d5d8cef22786982e924de60e47441833cedbfca
Ivac98 <xx89nacho98xx@gmail.com>
2024-03-17 02:33:04 -0300

    revork

--- js/index.js
+++ js/index.js
@@ -1,119 +1,138 @@
-class Moneda {
-    constructor(simbolo, nombre, valorCompra, valorVenta) {
-        this.simbolo = simbolo
-        this.nombre = nombre
-        this.valorCompra = valorCompra
-        this.valorVenta = valorVenta
-    }
+let moneda = document.getElementById('moneda');
+let change = document.getElementById('change-type');
+let monto = document.getElementById('monto');
+let montoOp = document.getElementById('monto-op');
+let taza = document.getElementById('taza');
+let realizar = document.getElementById('realizar')
+let error = document.getElementById('error')
+
+
+
+function realizarCambio() {
+    fetch(`https://v6.exchangerate-api.com/v6/397d1bd4552b3a3c3315c574/latest/${moneda.value}`)
+        .then(res => res.json())
+        .then(data => {
+            let cambio = data.conversion_rates.ARS;
+            if (monto.value >= 1) {
+                realizar.disabled = false
+                error.style.display = "none"
+                let montoCurrency = new Intl.NumberFormat("es-sp").format(monto.value)
+                console.log(montoCurrency)
+                if (change.value == "comprar") {
+                    let montoFinal = (monto.value * (cambio * 0.95)).toFixed(2);
+                    let montoCurrency2 = new Intl.NumberFormat("es-sp").format(montoFinal);
+                    console.log(montoFinal)
+                    taza.innerText = `1 ${moneda.value} = ${(cambio * 0.95).toFixed(5)} ARS`
+                    realizar.addEventListener("click", function () {
+                        montoOp.innerText = `Compra realizada: ${montoCurrency} ${moneda.value} por un valor de $${montoCurrency2} ARS`
+                        localStorage.setItem('localHist', montoOp.outerText);
+                        const Toast = Swal.mixin({
+                            toast: true,
+                            position: "top-end",
+                            showConfirmButton: false,
+                            timer: 2000,
+                            color: "#2d2d2d",
+                            background: "#8598cd",
+                            didOpen: (toast) => {
+                                toast.onmouseenter = Swal.stopTimer;
+                                toast.onmouseleave = Swal.resumeTimer;
+                            }
+                        });
+                        Toast.fire({
+                            icon: "success",
+                            title: "Transaccion realizada"
+                        });
+                    })
+                } else if (change.value == "vender") {
+                    let montoFinal = (monto.value * (cambio * 1.05)).toFixed(2);
+                    let montoCurrency2 = new Intl.NumberFormat("es-sp").format(montoFinal);
+                    taza.innerText = `1 ${moneda.value} = ${(cambio * 1.05).toFixed(5)} ARS`
+                    realizar.addEventListener("click", function () {
+                        montoOp.innerText = `Venta realizada: ${montoCurrency} ${moneda.value} por un valor de $${montoCurrency2} ARS`
+                        localStorage.setItem('localHist', montoOp.outerText);
+                        const Toast = Swal.mixin({
+                            toast: true,
+                            position: "top-end",
+                            showConfirmButton: false,
+                            timer: 2000,
+                            color: "#2d2d2d",
+                            background: "#8598cd",
+                            didOpen: (toast) => {
+                                toast.onmouseenter = Swal.stopTimer;
+                                toast.onmouseleave = Swal.resumeTimer;
+                            }
+                        });
+                        Toast.fire({
+                            icon: "success",
+                            title: "Transaccion realizada"
+                        });
+                    })
+                }
+            }
+            else {
+                realizar.disabled = true
+                error.style.display = "block"
+            }
+        })
 };
-const MonedasArray = [
-    Dolar = new Moneda("USD$", "Dolar", 825.50, 895.00),
-    Euro = new Moneda("€", "Euro", 890.00, 960.00),
-    Real = new Moneda("R$", "Real", 153.06, 202.94),
-    Libra = new Moneda("£", "Libra", 924.40, 1023.00),
-    Uruguayo = new Moneda("UYU$", "Uruguayo", 19.50, 20.00),
-    Chileno = new Moneda("CLP$", "Chileno", 85.65, 85.76),
-];
-
-const MonedaCotizacion = document.getElementById('moneda');
-const monedaSelect = document.getElementById('monedaSelect');
-function AgregarMoneda(monedas){
-    MonedasArray.forEach(monedas =>{
-        const monedaCot = document.createElement('div');
-        monedaCot.classList.add("tipoMoneda");
-        monedaCot.innerHTML =
-            `
-            <p class="monedaSimbolo">${monedas.simbolo}</p>
-            <h3 class="monedaNombre">${monedas.nombre}</h3>
-            <p class="monedaCompra">$${monedas.valorCompra}</p>
-            <p class="monedaVenta">$${monedas.valorVenta}</p>
-            `
-        MonedaCotizacion.appendChild(monedaCot);
-    })
-    MonedasArray.forEach(monedas =>{ 
-        let monedaSel = document.createElement('option');
-        monedaSel.setAttribute("value", monedas.nombre);
-        monedaSel.innerHTML =
-        `
-        ${monedas.nombre} (${monedas.simbolo})
-        `
-        monedaSelect.appendChild(monedaSel);
-    })
-};
-AgregarMoneda(MonedasArray);
-
-const cYv = document.getElementById('compra-venta');
-const monto = document.getElementById('monto');
-const resultado = document.getElementById('resultado');
-const click = document.getElementById('click');
-
-let tipoDeMoneda;
-monedaSelect.addEventListener("change", function(){
-    tipoDeMoneda = monedaSelect.options[monedaSelect.selectedIndex].value;
-    if (tipoDeMoneda == "Dolar"){
-        tipoDeMoneda = Dolar;
-    }else if(tipoDeMoneda == "Euro"){
-        tipoDeMoneda = Euro;
-    }else if(tipoDeMoneda == "Real"){
-        tipoDeMoneda = Real;
-    }else if(tipoDeMoneda == "Libra"){
-        tipoDeMoneda = Libra;
-    }else if(tipoDeMoneda == "Chileno"){
-        tipoDeMoneda = Chileno;
-    }else if(tipoDeMoneda == "Uruguayo"){
-        tipoDeMoneda = Uruguayo;
-    }
-})
-let tipoDeCambio; 
-cYv.addEventListener("change", function(){
-    tipoDeCambio = cYv.options[cYv.selectedIndex].value;
-})
-let tipoDeMonto; 
-monto.addEventListener("input", function(){
-    tipoDeMonto = Number(monto.value);
-})
-
-let montoMoneda;
-let valorFinal;
-const resultadoTipo = document.getElementById('resultadoTipo')
-click.addEventListener('click', function(){
-    if(tipoDeCambio == "comprar" && tipoDeMonto >= 100){   
-        let montoMoneda = tipoDeMonto * tipoDeMoneda.valorVenta;
-        let valorFinal = Math.round(montoMoneda);
-        document.getElementById('resultadoOK').style.display = "block";
-        resultadoTipo.innerText = `Compró $${tipoDeMonto} ${tipoDeMoneda.nombre.toUpperCase()} a un cambio de $${tipoDeMoneda.valorVenta}, por un valor de ARS$${valorFinal}`
-        localStorage.setItem('localH', resultadoTipo.outerText)
-    }else if(tipoDeCambio == "vender" && tipoDeMonto >= 100){ 
-        let montoMoneda = tipoDeMonto * tipoDeMoneda.valorCompra;
-        let valorFinal = Math.round(montoMoneda);
-        document.getElementById('resultadoOK').style.display = "block";
-        resultadoTipo.innerText = `Vendió $${tipoDeMonto} ${tipoDeMoneda.nombre.toUpperCase()} a un cambio de $${tipoDeMoneda.valorCompra}, por un valor de ARS$${valorFinal}`;
-        localStorage.setItem('localH', resultadoTipo.outerText)
-    }else if(tipoDeMonto < 100){
-        alert("El monto minimo por operación es de $100")
-    }
-})
+realizarCambio();
+change.addEventListener('change', realizarCambio);
+moneda.addEventListener('change', realizarCambio);
+monto.addEventListener('input', realizarCambio);
 
 
 const historial = document.getElementById('historial');
-let historialText
-let fecha = new Date()
-let fecha2 = fecha.toLocaleString()
+let historialText;
+let fecha = new Date();
+let fecha2 = fecha.toLocaleString();
 
+historial.innerHTML = localStorage.getItem('localHist2')
+realizar.addEventListener('click', function () {
+    setTimeout((
+        function asd() {
+            let historialText = document.createElement('p');
+            historialText.innerHTML =
+                `
+            ${fecha2} --- ${localStorage.getItem('localHist')} ---
+            `
+            historial.appendChild(historialText);
+            localStorage.setItem('localHist2', historial.innerHTML);
+        }), 100)
+})
+document.getElementById('borrar').addEventListener('click', function () {
+    const swalWithBootstrapButtons = Swal.mixin({
+        customClass: {
+            confirmButton: "btn btn-success",
+            cancelButton: "btn btn-danger"
+        },
+        buttonsStyling: false
+    });
+    swalWithBootstrapButtons.fire({
+        title: "Estas seguro?",
+        text: "Estas por eliminar el historial!!",
+        icon: "warning",
+        color: "#2d2d2d",
+        background: "#8598cd",
+        showCancelButton: true,
+        confirmButtonText: "Borrar",
+        confirmButtonColor: "#2d2d2d",
+        cancelButtonText: "Cancelar",
+        cancelButtonColor: "#2d2d2d",
+        reverseButtons: true
+    }).then((result) => {
+        if (result.isConfirmed) {
+            historial.innerHTML =
+                `
+                `
+            localStorage.removeItem('localHist2');
+            swalWithBootstrapButtons.fire({
+                title: "Borrado!",
+                text: "Tu historial ha sido borrado",
+                icon: "success",
+                color: "#2d2d2d",
+                background: "#8598cd"
+            });
+        }
+    });
+})
 
-historial.innerHTML = localStorage.getItem('localH2')
-click.addEventListener('click', function(){
-    let historialText = document.createElement('p');
-    historialText.innerHTML = 
-    `
-    ${fecha2} ${localStorage.getItem('localH')}
-    `
-    historial.appendChild(historialText);
-    localStorage.setItem('localH2', historial.innerHTML);
-})
-document.getElementById('borrarH').addEventListener('click', function(){
-    historial.innerHTML=
-    `
-    `
-    localStorage.removeItem('localH2')
-})
